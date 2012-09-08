c3660ab79c9e0af63e97236bc55af09588d5ab60
Jiri Slaby <jirislaby@gmail.com>
2012-09-08 13:01:44 +0200

    scripts: build-fix, some improvements
    To catch more.
    
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

--- scripts/build-fix.sh
+++ scripts/build-fix.sh
@@ -3,12 +3,15 @@
 SET=$1
 
 for FILE in `cat "$SET"`; do
-	sed -i	-e 's@^long __builtin_expect(long val , long res ) $@long s__builtin_expect(long val , long res ) @' \
-		-e 's@^void assert(int i ) $@void sassert(int i)@' \
-		-e 's@^void \*__builtin_memcpy(void \* , void    \* , unsigned long  ) ;$@@' \
-		-e 's@^unsigned long __builtin_object_size(void \* , int *) ;$@@' \
-		-e 's@^long __builtin_expect(long , long ) ;$@@' \
-		-e 's@^void __builtin_va_start(__builtin_va_list *) ;$@@' \
-		-e 's@void \*__builtin_alloca(unsigned int  ) ;@@' \
-		-e 's@__builtin_va_start(\(.*\));@va_start(\1);@' "$FILE"
+	sed -i	-e '
+	s@^long __builtin_expect(long val , long res ) $@long s__builtin_expect(long val , long res ) @;
+	s@^void assert(int i ) $@void sassert(int i)@;
+	s@^void \*__builtin_memcpy(void \* , void[ const]*\* , unsigned long *) *;$@@;
+	s@^unsigned long __builtin_object_size(void \* , int *) ;$@@;
+	s@^long __builtin_expect(long , long ) ;$@@;
+	s@^void __builtin_prefetch(void const *\* *, \.\.\.) ;@@;
+	s@^void __builtin_va_start(__builtin_va_list *) ;$@@;
+	s@^void __builtin_va_end(__builtin_va_list *) ;$@@;
+	s@^void \*__builtin_alloca(unsigned [longit]* *) ;$@@;
+	s@__builtin_va_start(\(.*\));@va_start(\1);@' "$FILE"
 done
