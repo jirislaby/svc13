f99dad7685cd525733536485266d28cbfa17c69e
Jiri Slaby <jirislaby@gmail.com>
2012-09-09 18:16:56 +0200

    scripts: instrument, improve
    To have an assert in all files.
    
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

--- scripts/instrument.sh
+++ scripts/instrument.sh
@@ -3,5 +3,10 @@
 SET=$1
 
 for FILE in `cat "$SET"`; do
-	sed -i 's/^\s*\<ERROR:/& assert(0);/' "$FILE"
+	sed -i -e '
+	s@^\s*\<ERROR:[ ;]*$@& assert(0);@;
+	s@^\s*\<ERROR: *return@ERROR: assert(0); return@;
+	s@^\s*{* *ERROR: goto ERROR; *}*\s*$@{ ERROR: assert(0); goto ERROR; }@;
+	s@^\s*\(if (.*)\) ERROR: goto ERROR;;*\s*$@\1 { ERROR: assert(0); goto ERROR; }@;
+	s@;\s*\<ERROR:@& assert(0);@' "$FILE"
 done
