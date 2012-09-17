f23dc9cbc3e07db3f123daf359805f55fd15eeaf
Jiri Slaby <jirislaby@gmail.com>
2012-09-17 16:08:42 +0200

    scripts: build-fix, only declare __VERIFIER_nondet_int
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

--- scripts/build-fix.sh
+++ scripts/build-fix.sh
@@ -13,5 +13,7 @@ for FILE in `cat "$SET"`; do
 	s@^void __builtin_va_start(__builtin_va_list *) ;$@@;
 	s@^void __builtin_va_end(__builtin_va_list *) ;$@@;
 	s@^void \*__builtin_alloca(unsigned [longit]* *) ;$@@;
-	s@__builtin_va_start(\(.*\));@va_start(\1);@' "$FILE"
+	s@__builtin_va_start(\(.*\));@va_start(\1);@;
+	s@\(int __VERIFIER_nondet_int()\) { int x; return x; }@extern \1;@' \
+		"$FILE"
 done
