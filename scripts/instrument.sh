23678dd31f135609bc1e8a58e0dbeb62b3227df6
Jiri Slaby <jirislaby@gmail.com>
2012-09-24 09:44:00 +0200

    scripts: instrument, remove implementation of __VERIFIER_assert
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

--- scripts/instrument.sh
+++ scripts/instrument.sh
@@ -8,5 +8,6 @@ for FILE in `cat "$SET"`; do
 	s@^\s*\<ERROR: *return@ERROR: assert(0); return@;
 	s@^\s*{* *ERROR: goto ERROR; *}*\s*$@{ ERROR: assert(0); goto ERROR; }@;
 	s@^\s*\(if (.*)\) ERROR: goto ERROR;;*\s*$@\1 { ERROR: assert(0); goto ERROR; }@;
+	s@^\(void __VERIFIER_assert(int expression, char\* x) {\) if (!expression) { ERROR: goto ERROR; }; return; }$@\1 assert(expression); }@;
 	s@;\s*\<ERROR:@& assert(0);@' "$FILE"
 done
