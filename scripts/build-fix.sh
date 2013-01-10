e716d97290f00758addbeb49f94f47ae12f21ba4
Jiri Slaby <jirislaby@gmail.com>
2013-01-10 22:24:16 +0100

    build-fix: remove some which are already fixed
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

--- scripts/build-fix.sh
+++ scripts/build-fix.sh
@@ -10,16 +10,13 @@ for FILE in $FILES; do
 	sed -i	-e '
 	s@__inline_*@@;
 	s@^long __builtin_expect(long val , long res ) $@long s__builtin_expect(long val , long res ) @;
-	s@^void assert(int i ) $@void sassert(int i)@;
+	s@^ *void assert(int i *) *$@void sassert(int i)@;
 	s@^void assert(int cond) {$@void sassert(int cond) {@;
 	s@^void \*__builtin_memcpy(void \* , void[ const]*\* , unsigned long *) *;$@@;
 	s@^unsigned long __builtin_object_size(void \* , int *) ;$@@;
 	s@^long __builtin_expect(long , long ) ;$@@;
 	s@^void __builtin_prefetch(void const *\* *, \.\.\.) ;@@;
-	s@^void __builtin_va_start(__builtin_va_list *) ;$@@;
-	s@^void __builtin_va_end(__builtin_va_list *) ;$@@;
 	s@^void \*__builtin_alloca(unsigned [longit]* *) ;$@@;
-	s@__builtin_va_start(\(.*\));@va_start(\1);@;
 	s@^\([a-z_ *]* __VERIFIER_nondet_[a-z_]*()\) { [a-z_ *]* [a-z_]*; return [a-z_]*; }@extern \1;@
 	s@^\(int __VERIFIER_nondet_int()\) {$@\1; int bubak() {@' \
 		"$FILE"
