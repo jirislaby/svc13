5a2b2a687f0c1752ee6497208881b9b67c4b0c1e
Statica project <statica@fi.muni.cz>
2015-02-15 09:16:10 +0100

    simplify instrumentation
    just insert __VERIFIER_error() after ERROR label

--- scripts/instrument.sh
+++ scripts/instrument.sh
@@ -9,9 +9,5 @@ fi
 for FILE in $FILES; do
 	sed -i -e '
 	s@__VERIFIER_assert(\(.*\),.*)@ __VERIFIER_assert(\1)@;
-	s@^\s*\<ERROR:[ ;]*$@& __VERIFIER_error();@;
-	s@^\s*\<ERROR: *return@ERROR: __VERIFIER_error(); return@;
-	s@^\s*{* *ERROR: goto ERROR; *}*\s*$@{ ERROR: __VERIFIER_error(); goto ERROR; }@;
-	s@^\s*\(if (.*)\) ERROR: goto ERROR;;*\s*$@\1 { ERROR: __VERIFIER_error(); goto ERROR; }@;
-	s@;\s*\<ERROR:@& __VERIFIER_error();@' "$FILE"
+	s@ERROR:@ERROR: __VERIFIER_error();@' "$FILE"
 done
