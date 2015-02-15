ead0edf95e9a8000d0fd56e3a0450daf04445eea
Statica project <statica@fi.muni.cz>
2015-02-15 10:05:32 +0100

    add one more case

--- scripts/instrument.sh
+++ scripts/instrument.sh
@@ -9,5 +9,6 @@ fi
 for FILE in $FILES; do
 	sed -i -e '
 	s@__VERIFIER_assert(\(.*\),.*).*@ __VERIFIER_assert(\1);@;
+	s@ERROR:\s*goto ERROR;@ERROR: __VERIFIER_error();@;
 	s@ERROR:@ERROR: __VERIFIER_error();@' "$FILE"
 done
