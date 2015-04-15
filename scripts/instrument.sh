45c5f8f6eec8304b848d9679a7779f43edf1c0db
Statica project <statica@fi.muni.cz>
2015-04-15 14:56:35 +0200

    instrument: do not replace __VERIFIER_assert

--- scripts/instrument.sh
+++ scripts/instrument.sh
@@ -8,7 +8,6 @@ fi
 
 for FILE in $FILES; do
 	sed -i -e '
-	s@__VERIFIER_assert(\(.*\),.*).*@ __VERIFIER_assert(\1);@;
 	s@ERROR:\s*goto ERROR;@ERROR: __VERIFIER_error();@;
 	s@ERROR:@ERROR: __VERIFIER_error(); __VERIFIER_error();@' "$FILE"
 done
